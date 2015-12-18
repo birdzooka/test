af4d8c2809f88deb13cf95643e4ef739b6c15eea
Deon Ma <deonm@uci.edu>
2015-12-17 21:10:26 -0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Deon Ma on 12/17/15.
+//  Copyright (c) 2015 ARAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  test
+//
+
+//  Copyright (c) 2015 ARAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
